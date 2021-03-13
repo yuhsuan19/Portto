1d22d54be32a3400d79ce39dc36a3948b3521cdf
Shane <shane.chi@peronaad.com>
2021-03-14 00:51:41 +0800

    [InfuraAPI] Create network serveice

---
+++ Portto/Common/NetworkAPI/InfuraAPI.swift
@@ -0,0 +1,58 @@
+//
+//  InfuraAPI.swift
+//  Portto
+//
+//  Created by CHI on 2021/3/14.
+//
+
+import Foundation
+
+enum InfuraAPI {
+    case getBalance
+}
+
+extension InfuraAPI: NetworkService {
+    var baseURL: URL {
+        return URL(string: "https://mainnet.infura.io/v3/")!
+    }
+    
+    var path: String {
+        return "0f4c28c65af942219e3da6aa3b22783c"
+    }
+    
+    var method: HTTPMethod {
+        return .post
+    }
+    
+    var httpHeaders: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+    
+    var parameters: [String : Any?]? {
+        switch self {
+        case .getBalance:
+            return [
+                "jsonrpc": "2.0",
+                "method": "eth_getBalance",
+                "params": ["0x960DE9907A2e2f5363646d48D7FB675Cd2892e91", "latest"],
+                "id": 1
+            ]
+        }
+    }
+    
+    var encoding: Encoding? {
+        .json
+    }
+    
+    var timeout: TimeInterval {
+        15
+    }
+    
+    var uploadContent: UploadContent? {
+        return nil
+    }
+    
+    
+}
